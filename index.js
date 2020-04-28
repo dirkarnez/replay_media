aa36053f88c06ae13ef5dd638f747668cc3dd6de
dirkarnez <smalldirkalex@gmail.com>
2020-04-28 08:23:29 +0800

    - add style

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-document.getElementsByTagName("video")[0].loop = true;
-document.getElementsByTagName("video")[0]
+var player = document.getElementsByTagName("video")[0];
+player.loop = true;
+player.style.width = "98%";
+player.style.height = "54px";
+player.style.borderStyle = "solid";
+player.style.backgroundColor = "#F1F3F4";
+player.style.borderRadius = "5px";
+player
 .play()
 .then(() => console.log("replaying"))
-.catch(err => console.log("error cannot replay:", err))
+.catch(err => console.log("error cannot replay:", err));
