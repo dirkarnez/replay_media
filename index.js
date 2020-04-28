bedac94bfc8b1015db015c2bd6acb251531c7936
dirkarnez <smalldirkalex@gmail.com>
2020-04-28 11:07:56 +0800

    - add player

--- index.js
+++ index.js
@@ -1,11 +1,131 @@
+function addToHistory(decodedSourceSrc) {
+    var history = getHistory();
+    history.push({listenedAt: new Date().toDateString(), songFile: decodedSourceSrc })
+    localStorage.setItem("history", JSON.stringify(history))
+}
+
+function getHistory() {
+    var history = JSON.parse(localStorage.getItem("history"));
+    return Array.isArray(history) ? history : [];
+}
+
+function play(decodedSourceSrc) {
+    var footer = document.querySelector(".footer")
+    footer.innerHTML = "";
+
+    var newSource = document.createElement("source");
+    newSource.src = encodeURI(decodedSourceSrc);
+
+    var newPlayer = document.createElement("video");
+    newPlayer.className = "player";
+    newPlayer.loop = true;
+    newPlayer.controls = true;
+    newPlayer.appendChild(newSource);
+
+    footer.appendChild(newPlayer);
+    newPlayer
+    .play()
+    .then(() => console.log("replaying"))
+    .catch(err => console.log("error cannot replay:", err));
+}
+
 var player = document.getElementsByTagName("video")[0];
-player.loop = true;
-player.style.width = "98%";
-player.style.height = "54px";
-player.style.borderStyle = "solid";
-player.style.backgroundColor = "#F1F3F4";
-player.style.borderRadius = "5px";
-player
-.play()
-.then(() => console.log("replaying"))
-.catch(err => console.log("error cannot replay:", err));
+player.pause();
+
+var decodedSourceSrc = decodeURI(document.getElementsByTagName("source")[0].src);
+
+document.body.innerHTML = `
+<nav>
+  <h2>History <button onclick="localStorage.removeItem('history')">Delete all history</button></h2>
+  <ul id="history">
+  </ul>
+</nav>
+
+<article>
+  <h2>Lorem Ipsum</h2>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </p>
+</article>
+
+<footer class="footer">
+</footer>
+`;
+
+addToHistory(decodedSourceSrc);
+play(decodedSourceSrc);
+
+getHistory().forEach(item => {
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(item.songFile + " at " + item.listenedAt));
+    li.addEventListener("click", function () {
+        addToHistory(item.songFile);
+        play(item.songFile);
+    });
+    document.getElementById("history").appendChild(li);
+})
+
+addStyle(`
+    body {
+        background-color: unset;
+        margin: 0px;
+        height: 100vh;
+        display: grid;
+        grid-template-areas: 'nav main'
+                            'footer footer';
+        grid-template-columns: minmax(auto, 12em) 5fr;
+        grid-template-rows: auto 1fr auto;
+    }
+
+    footer {
+        height: 54px;
+        grid-area: footer;
+    }
+
+    nav {
+        grid-area: nav;
+    }
+
+
+    article {
+        grid-area: main;
+    }
+
+    * {
+        box-sizing: border-box;
+    }
+
+    footer {
+        background: #F1F3F4;
+    }
+
+    nav {
+        border-right: 1px solid lightgray;
+    }
+
+    nav, article {
+        overflow-y: auto;
+    }
+    
+    nav, article {
+        padding: 1rem;
+    }
+
+    .player {
+        position: unset;
+        width: 100%;
+        height: 54px;
+        background-color: #F1F3F4;
+        border-radius: 5px;
+    }
+`);
+
+/**
+ * Utility function to add CSS in multiple passes.
+ * @param {string} styleString
+ */
+function addStyle(styleString) {
+    const style = document.createElement('style');
+    style.textContent = styleString;
+    document.head.append(style);
+}
